5889c17d721816d68a84160531ce45e7ca784ff2
phdashik <princedhenshaw@gmail.com>
2022-09-09 20:41:54 -0700

    #include <stdlib.h>

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,9 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
-
-
 /**
-
 * main - print if the number is postive, zero, or negative
-
-*
 
 * Description: using the main function
 
