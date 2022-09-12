100ef833ec39d7a36c1999b823ac2b5691629357
phdashik <princedhenshaw@gmail.com>
2022-09-12 15:09:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 
- */
-int main(void)
+* Description: main - Prints out a string to stdout.
+* Return: 0 
+*/
+int main(void}
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
