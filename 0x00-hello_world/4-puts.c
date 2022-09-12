9839cdb08e638ae7d52cb2dfb1c0ecc833eb2d16
phdashik <princedhenshaw@gmail.com>
2022-09-12 16:13:01 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - print the string in the put function
  *
- * Description: using the mail function
+ * Description: using the main function
  * this program prints "programming is like building a multilingual puzzle
  * Return: 0
  */
-int mail(void)
+int main(void)
 {
 	puts{"\"Programming is like building a multilingual puzzle");
 
